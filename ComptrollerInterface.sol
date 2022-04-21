77cfd38f496352c49e2a7e769d42967387f34471
USB <cerusb@pm.me>
2022-04-21 13:09:56 +0800

    +dependency

---
+++ ComptrollerInterface.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.5.16;
+
+contract ComptrollerInterface {
+    /// @notice Indicator that this is a Comptroller contract (for inspection)
+    bool public constant isComptroller = true;
+
+    /*** Assets You Are In ***/
+
+    function enterMarkets(address[] calldata kTokens) external returns (uint[] memory);
+    function exitMarket(address kToken) external returns (uint);
+
+    /*** Policy Hooks ***/
+
+    function mintAllowed(address kToken, address minter, uint mintAmount) external returns (uint);
+    function mintVerify(address kToken, address minter, uint mintAmount, uint mintTokens) external;
+
+    function redeemAllowed(address kToken, address redeemer, uint redeemTokens) external returns (uint);
+    function redeemVerify(address kToken, address redeemer, uint redeemAmount, uint redeemTokens) external;
+
+    function borrowAllowed(address kToken, address borrower, uint borrowAmount) external returns (uint);
+    function borrowVerify(address kToken, address borrower, uint borrowAmount) external;
+
+    function repayBorrowAllowed(
+        address kToken,
+        address payer,
+        address borrower,
+        uint repayAmount) external returns (uint);
+    function repayBorrowVerify(
+        address kToken,
+        address payer,
+        address borrower,
+        uint repayAmount,
+        uint borrowerIndex) external;
+
+    function liquidateBorrowAllowed(
+        address kTokenBorrowed,
+        address kTokenCollateral,
+        address liquidator,
+        address borrower,
+        uint repayAmount) external returns (uint);
+    function liquidateBorrowVerify(
+        address kTokenBorrowed,
+        address kTokenCollateral,
+        address liquidator,
+        address borrower,
+        uint repayAmount,
+        uint seizeTokens) external;
+
+    function seizeAllowed(
+        address kTokenCollateral,
+        address kTokenBorrowed,
+        address liquidator,
+        address borrower,
+        uint seizeTokens) external returns (uint);
+    function seizeVerify(
+        address kTokenCollateral,
+        address kTokenBorrowed,
+        address liquidator,
+        address borrower,
+        uint seizeTokens) external;
+
+    function transferAllowed(address kToken, address src, address dst, uint transferTokens) external returns (uint);
+    function transferVerify(address kToken, address src, address dst, uint transferTokens) external;
+
+    /*** Liquidity/Liquidation Calculations ***/
+
+    function liquidateCalculateSeizeTokens(
+        address kTokenBorrowed,
+        address kTokenCollateral,
+        uint repayAmount) external view returns (uint, uint);
+}
